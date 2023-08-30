64496e01c63bb8c3d6b14fb86af60a5de7d2fdb4
phik <phik.dev@gmail.com>
2023-08-30 20:17:01 +0200

    ajout sass

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/sass/style.css', array(), filemtime(get_stylesheet_directory() . '/sass/style.css'));
+    
 }
 
 // Get customizer options form parent theme
