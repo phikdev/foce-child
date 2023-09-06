e206752296442b38f9c113385ef244f74044ec1c
phik <phik.dev@gmail.com>
2023-09-06 20:43:19 +0200

    test keyframe

---
+++ template-parts/oscar.php
@@ -0,0 +1,4 @@
+<section id="oscar">
+                
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/perso.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+        </section>
